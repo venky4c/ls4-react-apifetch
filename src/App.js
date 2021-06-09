064ca58bf5feddf75a0e61d4e2c3d300f702eadb
Venky4c <59696420+venky4c@users.noreply.github.com>
2021-06-09 20:47:40 +0530

    Added search for users and blogs

--- src/App.js
+++ src/App.js
@@ -5,49 +5,71 @@ import User from "./User";
 
 export default function App() {
   const [users, setUsers] = React.useState([]);
-  const [searchUser, setSearchUser] = React.useState("");
+  const [searchUser, setSearchUser] = React.useState({});
   const [blogs, setBlogs] = React.useState([]);
+  const [searchBlog, setSearchBlog] = React.useState({});
   const userUrl = "https://jsonplaceholder.typicode.com/users";
   const blogUrl = "https://jsonplaceholder.typicode.com/posts";
   React.useEffect(() => {
-    fetch(userUrl)
-      .then((response) => response.json())
-      .then((res) => setUsers(res));
+    requestUsers();
   }, []);
-
+  const requestUsers = async () => {
+    const res = await fetch(userUrl);
+    const json = await res.json();
+    setUsers(json);
+  };
   React.useEffect(() => {
-    fetch(blogUrl)
-      .then((response) => response.json())
-      .then((res) => setBlogs(res));
+    requestBlogs();
   }, []);
 
-  const handleChange = (e) => {
-    setSearchUser(e.target.value);
-  };
-  const handleClick = (searchUser) => {
-    console.log(searchUser);
-    const searchTerm = users.filter((user) => user.name.includes(searchUser));
-    console.log("searching for .", searchTerm);
-    setUsers(searchTerm);
+  const requestBlogs = async () => {
+    const res = await fetch(blogUrl);
+    const json = await res.json();
+    setBlogs(json);
   };
 
+  const filterUsers = (event) => {
+    if (searchUser)
+      setUsers(users.filter((user) => user.name.includes(searchUser)));
+    event.preventDefault();
+  };
+
+  const filterBlogs = (event) => {
+    event.preventDefault();
+    if (searchBlog)
+      setBlogs(blogs.filter((blog) => blog.title.includes(searchBlog)));
+  };
   return (
     <div className="App">
       <div>
-        <p>
-          <span>User: &nbsp;</span>
-          <input type="text" name="user" onChange={handleChange} />
-          <button onClick={handleClick}>Search </button>
-          <label>You Searched FOR: {searchUser}</label>
-        </p>
-        <h3> Authors and Blogs</h3>
+        <form>
+          <p>
+            <h3> Authors and Blogs</h3>
+            <span>Users: &nbsp;</span>
+            <input
+              type="text"
+              name="user"
+              onChange={(e) => setSearchUser(e.target.value)}
+            />
+            <button onClick={filterUsers}>Search Users </button>
+            {users.map((user) => (
+              <User key={user.id} user={user} />
+            ))}
+          </p>
+          <p>
+            <span>Blogs: &nbsp;</span>
+            <input
+              type="text"
+              name="blog"
+              onChange={(e) => setSearchBlog(e.target.value)}
+            />
+            <button onClick={filterBlogs}>Search Users </button>
+            {blogs.map((blog) => (
+              <Blog key={blog.id} blog={blog} />
+            ))}
+          </p>
+        </form>
       </div>
-      {users.map((user) => (
-        <User key={user.id} user={user} />
-      ))}
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} />
-      ))}
     </div>
   );
 }
